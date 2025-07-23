d0a0ada7d47df2763f5944e67551f956df126012
smile-4-life <duc.tran911@hcmut.edu.vn>
2025-07-23 20:06:46 +0700

    feat(TextBuffer): implement all methods

--- TextBuffer.cpp
+++ TextBuffer.cpp
@@ -237,6 +237,240 @@ TextBuffer::~TextBuffer() {
 }
 
 // TODO: implement other methods of TextBuffer
+void TextBuffer::insert(char c) {
+    this->buffer.insertAt(this-> cursorPos, c);
+    this->History->clearRedo();
+    this->History->addAction("insert", this->cursorPos, c);
+    this-> cursorPos++;
+    return;
+}
+
+void TextBuffer::deleteChar() {
+    int deletedIndex = --this->cursorPos;
+    char deletedChar = this->buffer.get(deletedIndex);
+    buffer.deleteAt(deletedIndex);
+    this->History->clearRedo();
+    this->History->addAction("delete", deletedIndex+1, deletedChar);
+    return;
+}
+
+void TextBuffer::moveCursorLeft() {
+    this->History->addAction("move", this->cursorPos, 'L');
+
+    if (this-> cursorPos == 0)
+        throw cursor_error();
+    this-> cursorPos--;
+    return;
+}
+
+void TextBuffer::moveCursorRight() {
+    this->History->addAction("move", this->cursorPos, 'R');
+
+    if (this-> cursorPos == this-> buffer.size())
+        throw cursor_error();
+    this-> cursorPos++; 
+    return;
+}
+
+void TextBuffer::moveCursorTo(int index) {
+    if (index < 0 || index > this-> buffer.size())
+        throw out_of_range("Index is invalid!");
+    this->History->addAction("move", this->cursorPos, 'J');
+    this-> cursorPos = index;
+    this->History->indexJump.insertAtTail(index);
+    return;
+}
+
+string TextBuffer::getContent() const {
+    Node<char>* current = this->buffer.getHead();
+    string result;
+    while(current != nullptr) {
+        result += (current->data);
+        current = current->next;
+    }
+    return result;
+}
+
+int TextBuffer::getCursorPos() const {
+    return this-> cursorPos;
+}
+
+int TextBuffer::findFirstOccurrence(char c) const {
+    return this-> buffer.indexOf(c);
+}
+
+int* TextBuffer:: findAllOccurrences(char c, int&count) const {
+    count = 0;
+    int* arr = nullptr;
+    Node<char>* current = this-> buffer.getHead();
+    int currentIndex = 0;
+    while(current != nullptr) {
+        if(current-> data == c) {
+            addToArr(arr, currentIndex, ++count);
+        }
+        current = current-> next;
+        currentIndex++;
+    }
+    return arr;
+}
+
+
+///// SORT
+
+int charOrder(char c) {
+    if (c >= 'A' && c <= 'Z') {
+        return (c - 'A') * 2;
+    } else if (c >= 'a' && c <= 'z') {
+        return (c - 'a') * 2 + 1;
+    } else {
+        return 1000 + c;  // ký tự đặc biệt xếp cuối
+    }
+}
+
+template<typename T>
+int charOrder(T x) {
+    return 0;
+}
+
+template <typename T>
+Node<T>* DoublyLinkedList<T>::getMiddle(Node<T>* head) {
+    if (!head) return head;
+    Node<T>* slow = head;
+    Node<T>* fast = head;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+template <typename T>
+Node<T>* DoublyLinkedList<T>::sortedMerge(Node<T>* a, Node<T>* b) {
+    if (!a) return b;
+    if (!b) return a;
+
+    if (charOrder(a->data) <= charOrder(b->data)) {
+        a->next = sortedMerge(a->next, b);
+        if (a->next) a->next->prev = a;
+        a->prev = nullptr;
+        return a;
+    } else {
+        b->next = sortedMerge(a, b->next);
+        if (b->next) b->next->prev = b;
+        b->prev = nullptr;
+        return b;
+    }
+}
+
+template <typename T>
+Node<T>* DoublyLinkedList<T>::mergeSort(Node<T>* head) {
+    if (!head || !head->next) return head;
+
+    Node<T>* mid = getMiddle(head);
+    Node<T>* second = mid->next;
+    mid->next = nullptr;
+    if (second) second->prev = nullptr;
+
+    Node<T>* left = mergeSort(head);
+    Node<T>* right = mergeSort(second);
+
+    return sortedMerge(left, right);
+}
+
+void TextBuffer::sortAscending() {
+    string snapShot = this->getContent();
+    this->History->snapShot.insertAtTail(snapShot);
+
+    this->History->addAction("sort", this->cursorPos, '\0');
+    if (this->buffer.size() <= 1) return;
+
+    // Gọi merge sort
+    this->buffer.setHead ( this->buffer.mergeSort(this->buffer.getHead()) );
+
+    // Cập nhật lại tail
+    this->buffer.setTail ( this->buffer.getHead() );
+    while (this->buffer.getTail() && this->buffer.getTail()->next)
+        this->buffer.setTail( this->buffer.getTail()->next );
+
+    // Reset con trỏ về đầu
+    this->cursorPos = 0;
+    
+}
+
+///// END SORT
+
+void TextBuffer::deleteAllOccurrences(char c) {
+    Node<char>* current = this->buffer.getHead();
+    int index = 0;
+    while(current != nullptr) {
+        Node<char>* nextNode = current-> next;
+        if (current->data == c) {
+            this->buffer.deleteAt(index);
+            this->cursorPos--;
+        }
+        else {
+            index++;
+        }
+        current = nextNode;
+    }
+}
+
+void TextBuffer::undo() {
+    Action C = this->History->undoAction();
+    if (C.name == "insert") {
+        this->buffer.deleteAt(C.cursorPos);
+    }   
+    if (C.name == "delete") {
+        this->buffer.insertAt(C.cursorPos - 1, C.c);
+    }
+    if (C.name == "move") {
+
+    }
+    if (C.name == "sort") {
+        Node<string>* SnapShot = this->History->snapShot.getTail();
+        string snapShot = SnapShot->data;
+        this->History->snapShot.removeTail();
+        int i = 0;
+        Node<char>* current = this->buffer.getHead();
+        while(i < snapShot.length()) {
+            current->data = snapShot[i];
+            i = i + 1;
+            current = current->next;
+        }
+    }
+    if(C.name == "") {
+        return;
+    }
+    this->cursorPos = C.cursorPos;
+}
+
+void TextBuffer::redo() {
+    Action C = this->History->redoAction();
+    char c = C.c;
+    if (C.name == "insert") {
+        this->buffer.insertAt(C.cursorPos, c);
+        this->cursorPos = C.cursorPos+1;
+    }   
+    if (C.name == "delete") {
+        this->buffer.deleteAt(C.cursorPos-1);
+        this->cursorPos = C.cursorPos-1;
+    }
+    if (C.name == "move") {
+        if (C.c == 'L') this->cursorPos = C.cursorPos - 1;
+        else if (C.c == 'R') this->cursorPos = C.cursorPos + 1;
+        else {
+            Node<int>* indexNode = this->History->indexJump.getTail();
+            this->History->indexJump.removeTail();
+            this->cursorPos = indexNode->data;
+        }
+    }
+    if (C.name == "sort") {
+        this->sortAscending();
+    }
+    if (C.name == "") {
+        return;
+    }
+}
 
 // ----------------- HistoryManager -----------------
 TextBuffer::HistoryManager::HistoryManager() {
